9e08c83533a0b341dc789ad0d58ceb165dbf53e1
Nathen Johns <nathenjohns@yahoo.co.uk>
2018-09-05 11:00:54 +0000

    Index, Base HTMLs plus CSS and Background

--- app.py
+++ app.py
@@ -4,9 +4,12 @@ from flask import Flask, render_template, url_for, request, flash, redirect, ses
 app = Flask(__name__)
 app.secret_key = 'Keep it secret, keep it safe (LOTR not GOT)'
 
+#ROUTE FOR USER INSTRUCTIONS AND START OF QUIZ
 @app.route('/')
 def index():
-    return 'Hello world'
+    return render_template("index.html")
+    
+
 
 
 
